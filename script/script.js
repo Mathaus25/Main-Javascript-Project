03b530deab341757f4792f844bf0d5116c933495
Petrescu Matei <matei_petrescu@yahoo.com>
2022-07-16 21:07:30 +0300

    Changes regarding the JS file to have loop of 5

--- script/script.js
+++ script/script.js
@@ -1,31 +1,147 @@
-const choices = ["rock", "paper", "scissors"];
+(function () {
+  let playerScore = 0;
+  let computerScore = 0;
+  let gameStatus = "On";
+  let rounds = 5;
 
-const userSelection = prompt("Do you choose rock, paper or scissors?");
-if (userSelection === "paper" || userSelection === "rock" || userSelection === "scissors") {
-  console.log("You chose " + `${userSelection}`);
-} else {
-  console.log("Error! Try again!");
-}
 
-let computerSelection = choices[Math.floor(Math.random()*3)];
-console.log("Computer chose" + ` ${computerSelection}`);
+  const randomNumGenerator = () => {
+    let randomNum = Math.floor(Math.random() * 3);
+    return randomNum;
+  };
 
-const winner = game(userSelection, computerSelection);
+  const computerPlay = () => {
+    let gameKeyWords = ["rock", "paper", "scissors"];
+    return gameKeyWords[randomNumGenerator()];
+  };
 
-function game(userSelection, computerSelection) {
-  if (userSelection === 'rock' && computerSelection === 'paper') {
-    console.log('You lose! Paper beats Rock');
-  } else if (userSelection === 'rock' && computerSelection === 'scissors') {
-    console.log('You win! Rock beats Scissors');
-  } else if (userSelection === 'scissors' && computerSelection === 'rock') {
-    console.log('You lose! Rock beats Scissors');
-  } else if (userSelection === 'scissors' && computerSelection === 'paper') {
-    console.log('You win! Scissors beats Paper');
-  } else if (userSelection === 'paper' && computerSelection === 'scissors') {
-    console.log('You lose! Scissors beats Paper');
-  } else if (userSelection === 'paper' && computerSelection === 'rock') {
-    console.log('You win! Paper beats Rock');
-  } else {
-    console.log('You tie!');
+  const stringSanitizer = str => {
+    return str.trim().toLowerCase();
+  };
+
+  const roundWinnerChecker = (sanitizedPlayerSelection, computerSelection) => {
+    if (sanitizedPlayerSelection === computerSelection) {
+      return console.log("Draw!");
+    }
+
+    if (computerSelection === "rock") {
+      switch (sanitizedPlayerSelection) {
+        case "paper":
+          playerScore++;
+          return console.log(
+            `You Win! ${sanitizedPlayerSelection} beats ${computerSelection}`
+          );
+          break;
+        case "scissors":
+          computerScore++;
+          return console.log(
+            `You Lose! ${computerSelection} beats ${sanitizedPlayerSelection}`
+          );
+          break;
+      }
+    } else if (computerSelection === "paper") {
+      switch (sanitizedPlayerSelection) {
+        case "scissors":
+          playerScore++;
+          return console.log(
+            `You Win! ${sanitizedPlayerSelection} beats ${computerSelection}`
+          );
+          break;
+        case "rock":
+          computerScore++;
+          return console.log(
+            `You Lose! ${computerSelection} beats ${sanitizedPlayerSelection}`
+          );
+          break;
+      }
+    } else {
+      switch (sanitizedPlayerSelection) {
+        case "rock":
+          playerScore++;
+          return console.log(
+            `You Win! ${sanitizedPlayerSelection} beats ${computerSelection}`
+          );
+          break;
+        case "paper":
+          computerScore++;
+          return console.log(
+            `You Lose! ${computerSelection} beats ${sanitizedPlayerSelection}`
+          );
+          break;
+      }
+    }
+  };
+
+  const playRound = () => {
+    let playerSelection = prompt(
+      "Lets play! Choose between rock, paper and scissors",
+      "Rock"
+    );
+
+    if (!playerSelection) {
+      rounds++
+      return alert(
+        "Value can't be empty. Try rock," +
+          " paper and scissors"
+      );
+    }
+
+    let sanitizedPlayerSelection = stringSanitizer(playerSelection);
+
+    if (
+      !["rock", "paper", "scissors"].some(el => el === sanitizedPlayerSelection)
+    ) {
+      rounds++
+      return alert(
+        `${sanitizedPlayerSelection} is an invalid value ` +
+          `,only accepted values are rock, paper and scissors`
+      );
+    }
+
+    let computerSelection = computerPlay();
+
+    roundWinnerChecker(sanitizedPlayerSelection, computerSelection);
+  };
+
+  const gameStatusChecker = () => {
+    if (computerScore === playerScore) {
+      let playAgain = prompt("Draw! Play again? Enter Yes");
+
+      if (!playAgain) {
+        gameStatus = "See you!";
+        return;
+      }
+
+      playAgain = stringSanitizer(playAgain);
+
+      if (playAgain !== "yes") {
+        gameStatus = "See you!";
+        return console.log("See you!");
+      }
+      gameStatus = "On";
+      rounds = 5;
+      return;
+    }
+    gameStatus = "End of the game.";
+  };
+
+  const game = () => {
+    for (let i = 0; i < rounds; i++) {
+      playRound();
+      console.log(
+        `PlayerScore is ${playerScore}, computerScore is ${computerScore}`
+      );
+    }
+    gameStatusChecker();
+  };
+
+  while (gameStatus === "On") {
+    game();
   }
-}
+
+  if (computerScore > playerScore) {
+    console.log(`You lost: ${playerScore}:${computerScore}`);
+  } else if (playerScore > computerScore) {
+    console.log(`You won: ${playerScore}:${computerScore}`);
+  }
+})();
